566a610ac6b0637d477111c3b7dce0f75cbc2308
Juan Carlos <mrfpgapro@gmail.com>
2022-06-12 16:47:07 -0700

    June 6, 2022  -  4:47 PM

---
+++ TreeViewExample/WpfAppTreeView/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace WpfAppTreeView
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create ViewModel
+            WpfAppTreeViewModel VM = new WpfAppTreeViewModel();
+
+            // Show View Window through ViewModel
+            VM.showWindow();
+        }
+    }
+}
--- TreeViewExample/WpfAppTreeView/WpfAppTreeViewModel.cs
+++ TreeViewExample/WpfAppTreeView/WpfAppTreeViewModel.cs
@@ -8,6 +8,19 @@ namespace WpfAppTreeView
 {
     public class WpfAppTreeViewModel
     {
+        private MainWindow _view;
+
+        public void showWindow()
+        {
+            // Create the startup window
+            MainWindow wnd = new MainWindow();
+
+            // Do stuff here, e.g. to the window
+            wnd.Title = "Something else";
+
+            // Show the window
+            wnd.Show();
+        }
 
     }
 }
