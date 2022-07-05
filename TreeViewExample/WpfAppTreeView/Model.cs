929bba97a4afa176e9b6b3937edece23eed57fab
Juan Carlos <mrfpgapro@gmail.com>
2022-07-04 19:08:49 -0700

    July 4, 2022  -  7:08 PM

---
+++ TreeViewExample/WpfAppTreeView/Model.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace WpfAppTreeView
+{
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    public class TreeViewDataStruct : ViewModelBase
+    {
+        // Constructor
+        public TreeViewDataStruct()
+        {
+            // Initialize private members
+            _myTreeItems = new ObservableCollection<Tuple<State, City>>();
+            _myTreeDataStruct = new ObservableCollection<Tuple<State, List<City>>>();
+        }
+
+        private ObservableCollection<Tuple<State, List<City>>>? _myTreeDataStruct;
+        public ObservableCollection<Tuple<State, List<City>>>? MyTreeDataStruct
+        {
+            get => _myTreeDataStruct;
+            set
+            {
+                _myTreeDataStruct = value;
+                OnPropertyChanged("MyTreeDataStruct");
+            }
+        }
+
+        private ObservableCollection<Tuple<State, City>>? _myTreeItems;
+        public ObservableCollection<Tuple<State, City>>? MyTreeItems
+        {
+            get => _myTreeItems;
+            set
+            {
+                _myTreeItems = value;
+                OnPropertyChanged("MyTreeItems");
+            }
+        }
+
+        public void CompileTreeStructure()
+        {
+            // Return a string type, instead of a <State> object list. Filtering
+            // by string instead of object. This helps with filtering later
+            var statesQuery =
+                from items in _myTreeItems
+                select items.Item1.StateName;
+
+            // I could only use Distinct() with string types. I must conduct further research
+            // on how to accomplish using Distinct() with a custom class or object.
+            IEnumerable<string> temp = statesQuery.Distinct();
+
+            // Create a new list of unique states that are
+            // associated with each "Child" city
+            var statesList = new List<State>();
+            foreach (var item in temp)
+            {
+                statesList.Add(new State(item));
+            }
+
+
+            var tempCities = new List<City>();
+            foreach (var itemState in statesList)
+            {
+                tempCities.Clear();
+                foreach (var itemTree in _myTreeItems)
+                {
+                    if (itemState.StateName  ==  itemTree.Item1.StateName)
+                    {
+                        tempCities.Add(itemTree.Item2);
+                    }
+                }
+                _myTreeDataStruct?.Add(new Tuple<State, List<City>>(itemState, tempCities));
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    public class City : ViewModelBase
+    {
+        public City(string cityname)
+        {
+            CityName = cityname;
+        }
+
+        private string? _strCityName = string.Empty;
+        public string? CityName
+        {
+            get => _strCityName;
+            set
+            {
+                _strCityName = value;
+                OnPropertyChanged("CityName");
+            }
+        }
+    }
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    public class State : ViewModelBase
+    {
+        public State(string state)
+        {
+            StateName = state;
+        }
+
+        private string? _strStateName = string.Empty;
+        public string? StateName
+        {
+            get => _strStateName;
+            set
+            {
+                _strStateName = value;
+                OnPropertyChanged("StateName");
+            }
+        }
+    }
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+    //----------------------------------------------------------------------------
+}
--- TreeViewExample/WpfAppTreeView/WpfAppTreeViewModel.cs
+++ TreeViewExample/WpfAppTreeView/WpfAppTreeViewModel.cs
@@ -1,9 +1,65 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+// For De-Bugging
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace WpfAppTreeView
 {
     public class WpfAppTreeViewModel : ViewModelBase
     {
+        // Constructor
+        public WpfAppTreeViewModel()
+        {
+            _treeViewItemsList = new TreeViewDataStruct();
+            _treeViewItemsList.MyTreeItems?.Add( new Tuple<State, City>(new State("California"), new City("San Diego")) );
+            _treeViewItemsList.MyTreeItems?.Add( new Tuple<State, City>(new State("California"), new City("Sacramento")) );
+            _treeViewItemsList.MyTreeItems?.Add( new Tuple<State, City>(new State("California"), new City("Carlsbad")) );
+            _treeViewItemsList.MyTreeItems?.Add( new Tuple<State, City>(new State("Florida"), new City("Orlando")) );
+            _treeViewItemsList.MyTreeItems?.Add( new Tuple<State, City>(new State("Florida"), new City("Miami")) );
+            _treeViewItemsList.MyTreeItems?.Add( new Tuple<State, City>(new State("Florida"), new City("Jacksonville")) );
+            _treeViewItemsList.MyTreeItems?.Add( new Tuple<State, City>(new State("North Carolina"), new City("Raleigh")) );
+            _treeViewItemsList.MyTreeItems?.Add( new Tuple<State, City>(new State("North Carolina"), new City("Jacksonville")) );
+
+            // Create the TreeView Data Structure
+            _treeViewItemsList.CompileTreeStructure();
+
+
+            //MenuItem root = new MenuItem() { Title = "Menu" };
+            //MenuItem childItem1 = new MenuItem() { Title = "Child item #1" };
+            //childItem1.Items.Add(new MenuItem() { Title = "Child item #1.1" });
+            //childItem1.Items.Add(new MenuItem() { Title = "Child item #1.2" });
+            //root.Items.Add(childItem1);
+            //root.Items.Add(new MenuItem() { Title = "Child item #2" });
+            //trvMenu.Items.Add(root);
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (_treeViewItemsList.MyTreeItems != null)
+                    {
+                        Trace.WriteLine("\n");
+                        foreach (var itemState in _treeViewItemsList.MyTreeDataStruct)
+                        {
+                            Trace.WriteLine("");
+                            Trace.Write(itemState.Item1.StateName);
+                            foreach (var itemCity in itemState.Item2)
+                            {
+                                Trace.WriteLine("");
+                                Trace.Write(" |-- ");
+                                Trace.Write(itemCity.CityName);
+                            }
+                        }
+                        Trace.WriteLine("\n");
+                    }
+
+                    Thread.Sleep(1250);
+                }
+            });
+        }
+
         // View
         private MainWindow? _view;
 
@@ -13,72 +69,25 @@ namespace WpfAppTreeView
             // Create the startup window & initialize DataContext
             _view = new MainWindow()
             {
-                Title = "TreeView Example",
+                Title = "TreeView with Checkboxes Example",
                 DataContext = this
             };
 
             // Show the window
             _view.Show();
         }
-    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //-----------------------------------------------------------
-    //-----------------------------------------------------------
-    //-----------------------------------------------------------
-    //-----------------------------------------------------------
-    //-----------------------------------------------------------
-    //-----------------------------------------------------------
-    public class City : ViewModelBase
-    {
-        private string _strCityName = string.Empty;
-        public string CityName
+        private TreeViewDataStruct? _treeViewItemsList;
+        public TreeViewDataStruct? TreeViewItemsList
         {
-            get
-            {
-                return _strCityName;
-            }
+            get => _treeViewItemsList;
             set
             {
-                _strCityName = value;
-                OnPropertyChanged("CityName");
+                _treeViewItemsList = value;
+                OnPropertyChanged("TreeViewItemsList");
             }
         }
-
-        public City(string cityname)
-        {
-            CityName = cityname;
-        }
     }
-    //-----------------------------------------------------------
-    //-----------------------------------------------------------
-    //-----------------------------------------------------------
-    //-----------------------------------------------------------
-    //-----------------------------------------------------------
-    //-----------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
 
 
 
